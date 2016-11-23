d1ffe82c4f2722a59643a34ac217393f5df3f081
Oddclock <javiergarcia.escav@gmail.com>
2016-11-23 18:39:49 +0100

    Animación caja

---
+++ Assets/script/Generador.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generador : MonoBehaviour {
+	public GameObject[] Monedas;
+	private Transform pos_salida;
+	private GameObject moneda_nueva;
+	private int numero_moneda = 0;
+	public Animator anim;
+
+	void Start (){
+		pos_salida = transform.Find ("Posicion_salida").transform;
+		Debug.Log ("Cantidad de monedas: " + Monedas.Length);
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+		if (col.gameObject.tag == "Player" && moneda_nueva == null) {
+			numero_moneda = Random.Range (0, Monedas.Length-1);
+			moneda_nueva = (GameObject)Instantiate (Monedas[1],
+				pos_salida.position,
+				transform.rotation);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D objeto){
+		if (objeto.tag == "Player" && moneda_nueva == null) {
+			moneda_nueva = (GameObject)Instantiate (Monedas[1],
+				transform.position,
+				transform.rotation);
+		}
+
+	
+	}
+}
--- Assets/script/MenuScript.cs
+++ Assets/script/MenuScript.cs
@@ -5,46 +5,46 @@ using System.Collections;
 public class MenuScript : MonoBehaviour {
 
 	Animator anim;
-	private bool Start_menu = false;
+	private bool menu_pausa = false;
 
 	// Use this for initialization
 	void Start () {
-		
+
 		anim = GetComponent<Animator> ();
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			if (Start_menu) {
+		if (Input.GetKeyDown (KeyCode.Escape)){
+			if (menu_pausa) {
 				Reanudar ();
 			} else {
 				Pausa ();
-		
-	}
+			}
+		}
 
-	public void Pausa(){
-		Start_menu = false;
-		Time.TimeScale = 1;
-		anim.SetBool ("opciones", false);
-		anim.SetBool ("pausa", true);
-	}
+		public void Pausa(){
+			menu_pausa = false;
+			Time.TimeScale = 1;
+			anim.SetBool ("Opciones", false);
+			anim.SetBool ("Pausa", true);
+		}
 
-	public void Salir(){
-		Application.Quit ();
-		Debug.Log ("Saliendo");
-	}
+		public void Salir(){
+			Application.Quit ();
+			Debug.Log ("Saliendo");
+		}
 
-	public void Opciones(){
-		anim.SetBool ("opciones", true);
-		anim.SetBool ("pausa", true);
-	}
+		public void Opciones(){
+			anim.SetBool ("Opciones", true);
+			anim.SetBool ("Pausa", true);
+		}
 
-	public void Reanudar(){
-		menu_pausa = true;
-		anim.SetBool ("opciones", false);
-		anim.SetBool ("pausa", false);
+		public void Reanudar(){
+			menu_pausa = true;
+			anim.SetBool ("Opciones", false);
+			anim.SetBool ("Pausa", false);
 
+		}
 	}
-}
